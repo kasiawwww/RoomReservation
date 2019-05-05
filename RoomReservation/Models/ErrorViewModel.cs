81c2053a14f625f085c9a0c2976e6b7b7e4cf7df
kaska <kaska.widlak@gmail.com>
2019-05-05 22:04:56 +0200

    Add project files.

---
+++ RoomReservation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RoomReservation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
